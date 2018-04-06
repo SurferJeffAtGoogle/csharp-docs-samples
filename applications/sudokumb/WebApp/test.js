47656773960cbdc95a0b3db3ab81892cd53aa201
Jeffrey Rennie <rennie@google.com>
2018-04-06 09:53:23 -0700

    Test needs *stable* version of casperjs.
    Change-Id: I40487fb3269aabb0b7782077fd224a4eba6a25fd

--- applications/sudokumb/WebApp/test.js
+++ applications/sudokumb/WebApp/test.js
@@ -14,52 +14,91 @@
 
 var system = require('system');
 var host = system.env['CASPERJS11_URL'];
+console.log(host);
 
-currentCount = 0;
-
-casper.test.begin('Visit pages.', 6, function suite(test) {
+casper.test.begin('Visit pages.', 10, function suite(test) {
+    // Click the links in the top nav one by one.
     casper.start(host + '/', function (response) {
-        console.log('Starting ' + host + '/');
+        this.echo('Starting ' + response.url);
         test.assertEquals(200, response.status);
-        test.assertSelectorHasText('H1', 'Sudokumb');
+        test.assertSelectorHasText('title', 'Home Page - Sudokumb');
     });
 
     casper.thenClick('#nav-solve', function(response) {
+        this.echo('Visiting ' + response.url);        
         test.assertEquals(200, response.status);
-        test.assertSelectorHasText('H1', 'Solve');
+        test.assertSelectorHasText('title', 'Solve - Sudokumb');
     });
 
     casper.thenClick('#nav-admin', function(response) {
+        this.echo('Visiting ' + response.url);        
         test.assertEquals(200, response.status);
-        test.assertSelectorHasText('H1', 'Log in');
+        test.assertSelectorHasText('title', 'Log in - Sudokumb');
     });
 
+    casper.thenClick('#nav-home', function(response) {
+        this.echo('Visiting ' + response.url);        
+        test.assertEquals(200, response.status);
+        test.assertSelectorHasText('title', 'Home Page - Sudokumb');
+    });
+
+    // Then click the Try It! button.
+    casper.thenClick('.btn-primary', function(response) {
+        this.echo('Visiting ' + response.url);        
+        test.assertEquals(200, response.status);
+        test.assertSelectorHasText('title', 'Solve - Sudokumb');
+    });
+    
     /*
     casper.thenClick('#Submit', function (response) {
-        console.log('Submitted form.');
+        this.echo('Submitted form.');
         test.assertEquals(200, response.status);
         test.assertSelectorHasText('#LastIncrementedBy', 'test.js');
         test.assertSelectorHasText('#CurrentCount', currentCount + 1);
         this.fill('#WhoForm', {
             'Who': 'anothertest.js',
         }, false);
-        console.log("Filled another form.");
+        this.echo("Filled another form.");
     });
 
     casper.thenClick('#Submit', function (response) {
-        console.log('Submitted form.');
+        this.echo('Submitted form.');
         test.assertEquals(200, response.status);
         test.assertSelectorHasText('#LastIncrementedBy', 'anothertest.js');
     });
 
     casper.thenClick('#Reset', function (response) {
-        console.log('Clicked Reset.');
+        this.echo('Clicked Reset.');
         test.assertEquals(200, response.status);
         test.assertSelectorHasText('#CurrentCount', '0');
     });
+    */
 
     casper.run(function () {
         test.done();
     });
-    */
+});
+
+casper.test.begin('Submit puzzle', 3, function suite(test) {
+    casper.start(host + '/').thenClick('#nav-solve', function(response) {
+        this.echo('Visiting ' + response.url);        
+        test.assertEquals(200, response.status);
+    });
+
+    // Then click the Submit button.
+    casper.thenClick('.btn-primary', function(response) {
+        this.echo('Visiting ' + response.url);        
+        test.assertEquals(200, response.status);
+        test.assertSelectorHasText('#solvingMessage', 'Solving...');
+    });
+
+    casper.waitForSelectorTextChange('#solvingMessage', function() {
+        this.echo("Yeah, it's really solving.");
+    }, function() {
+        test.fail("Never updated solving status message.");
+    }, 4000);
+    
+    casper.run(function () {
+        test.done();
+    });
 });
