21a181ee64dfca0de11edb5a8653c5e8fc7779e8
Jeffrey Rennie <rennie@google.com>
2018-07-11 14:30:50 -0700

    WIP
    Change-Id: I9118c1c688bb5355cc26a5a5d93744035dd9435f

--- powershell/CopyTo-GceInstance.ps1
+++ powershell/CopyTo-GceInstance.ps1
@@ -1,3 +1,9 @@
-Param([string] $User, [string] $Password)
+Param([string] $User, [string] $Password, [string] $Instance)
 
+$tag = "$Instance-tcp-5986"
+
+gcloud compute instances add-tags $Instance --tags=$tag
+gcloud compute firewall-rules create --allow=tcp:5986 `
+    --description="Allow powershell remote sessions." `
+    --target-tags=$tag
 
