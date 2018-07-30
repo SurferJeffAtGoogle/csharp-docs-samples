fbc7d51cba43b69efefa8c53ebf0e42aa5eb7408
Jeffrey Rennie <rennie@google.com>
2018-07-30 16:07:16 -0700

    Test with fixture.
    Change-Id: Id34cbe3736df635ec5bf1e26982eae58b4695fc9

---
+++ monitoring/api/UptimeCheckTest/UptimeCheckTest.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace GoogleCloudSamples
+{
+
+
+    public class UptimeCheckTest : IClassFixture<UptimeCheckTestFixture>
+    {
+        private readonly UptimeCheckTestFixture _fixture;
+
+        public UptimeCheckTest(UptimeCheckTestFixture fixture)
+        {
+            _fixture = fixture;
+        }
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+
+    public class UptimeCheckTestFixture : IDisposable
+    {
+        public UptimeCheckTestFixture()
+        {
+            // Create two uptime checks to work with.
+            var output = Cmd.Run("create", "-p", ProjectId);
+            UptimeCheckConfigNames.Add(output.Stdout.Trim());
+            output = Cmd.Run("create", "-p", ProjectId);
+            UptimeCheckConfigNames.Add(output.Stdout.Trim());
+        }
+
+        public IList<string> UptimeCheckConfigNames { get; private set; } =
+            new List<string>();
+
+        public CommandLineRunner Cmd {get; private set;} = new CommandLineRunner()
+        {
+            VoidMain = UptimeCheck.Main,
+            Command = "UptimeCheck"
+        };
+
+        public string ProjectId { get; private set; } = 
+            Environment.GetEnvironmentVariable("GOOGLE_PROJECT_ID");
+
+        public void Dispose()
+        {
+            // Clean up the uptime checks we created:
+            foreach (string configName in UptimeCheckConfigNames)
+            {
+                Cmd.Run("delete", configName);
+            }
+        }
+    }
+}
