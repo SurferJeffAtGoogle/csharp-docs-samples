198950c445c92186a7c4931703a847eea6ba482c
Jeffrey Rennie <rennie@google.com>
2018-08-17 08:35:20 -0700

    Decrypt-AppSecrets
    Change-Id: I624db97a47002cc4aee76b07a08475759dcc2107

---
+++ appengine/flexible/SocialAuth/Decrypt-AppSecrets.ps1
@@ -0,0 +1,1 @@
+gcloud kms decrypt --plaintext-file appsecrets.json --ciphertext-file appsecrets.json.encrypted --key (Get-Content appsecrets.json.keyname) 
--- appengine/flexible/SocialAuth/Encrypt-AppSecrets.ps1
+++ appengine/flexible/SocialAuth/Encrypt-AppSecrets.ps1
@@ -1,1 +1,1 @@
-gcloud kms encrypt --plaintext-file .\appsecrets.json --ciphertext-file .\appsecrets.json.encrypted --key (Get-Content .\appsecrets.json.keyname) 
+gcloud kms encrypt --plaintext-file appsecrets.json --ciphertext-file appsecrets.json.encrypted --key (Get-Content appsecrets.json.keyname) 
