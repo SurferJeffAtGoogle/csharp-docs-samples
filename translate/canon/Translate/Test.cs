5558ac2d47e7a0ed74dd8b9bacfd6b370e10b15c
Jeffrey Rennie <rennie@google.com>
2018-09-04 16:10:14 -0700

    ListLanguageCodes
    Change-Id: Ic90b74b9f95f50199b77f33c4b063fc1d5b90025

--- translate/canon/Translate/Test.cs
+++ translate/canon/Translate/Test.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using Google.Cloud.Translation.V2;
 using Xunit;
 
 public class TranslateTest
@@ -6,9 +8,17 @@ public class TranslateTest
     TranslateSample sample = new TranslateSample();
 
     [Fact]
-    public void TranslateTextTest()
+    public void TestTranslateText()
     {
         string translatedText = sample.TranslateText();
         Assert.False(string.IsNullOrWhiteSpace(translatedText));
     }
+
+    [Fact]
+    public void TestListLanguageCodes()
+    {
+        IList<Language> codes = sample.ListLanguageCodes();
+        Assert.NotEmpty(codes);
+    }
+
 }
