23b6b50f0378608af70b7ac89d06c79eef0db948
Jeffrey Rennie <rennie@google.com>
2018-09-18 15:20:57 -0700

    Lint

--- applications/SocialAuthMVC/Startup.cs
+++ applications/SocialAuthMVC/Startup.cs
@@ -1,4 +1,18 @@
-﻿using Google.Apis.Auth.OAuth2;
+﻿// Copyright (c) 2018 Google LLC.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy of
+// the License at
+// 
+// http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
+
+using Google.Apis.Auth.OAuth2;
 using Google.Cloud.Diagnostics.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.DataProtection;
@@ -23,25 +37,25 @@ namespace SocialAuthMVC
 {
     public class Startup
     {
-        private Lazy<string> _projectId = new Lazy<string>(() => GetProjectId());
+        private readonly Lazy<string> _projectId = new Lazy<string>(() => GetProjectId());
         public string ProjectId
         {
             get { return _projectId.Value; }
         }
-        
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        private static string GetProjectId() 
+        private static string GetProjectId()
         {
             GoogleCredential googleCredential = Google.Apis.Auth.OAuth2
                 .GoogleCredential.GetApplicationDefault();
             if (googleCredential != null)
             {
                 ICredential credential = googleCredential.UnderlyingCredential;
-                ServiceAccountCredential serviceAccountCredential = 
+                ServiceAccountCredential serviceAccountCredential =
                     credential as ServiceAccountCredential;
                 if (serviceAccountCredential != null)
                 {
@@ -81,15 +95,16 @@ namespace SocialAuthMVC
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.Configure<Models.SecretsModel>(
                 Configuration.GetSection("Secrets"));
-                
+
             services.AddSingleton<Models.GoogleProjectModel>(
                 (provider) => new Models.GoogleProjectModel()
-            {
-                Id = ProjectId
-            });
+                {
+                    Id = ProjectId
+                });
             services.AddSingleton<IDataProtectionProvider,
                 KmsDataProtectionProvider>();
-            services.AddGoogleExceptionLogging(options => {
+            services.AddGoogleExceptionLogging(options =>
+            {
                 options.ProjectId = ProjectId;
                 options.ServiceName = "SocialAuthMVC";
                 options.Version = "0.01";
