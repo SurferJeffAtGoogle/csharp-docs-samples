574491dd5bf7f8f40ecdefa527b142579dc43342
Jeffrey Rennie <rennie@google.com>
2018-10-04 08:48:18 -0700

    GithubHook WIP.
    Change-Id: Ibcb35569e703f7b91dda2f93997c871864aaf32b

---
+++ pubsub/api/GithubHook/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace GithubHook.Controllers
+{
+    public class HookResponse {};
+
+    [Produces("application/json")]
+    public class HookController : ControllerBase
+    {
+        private readonly ILogger<HookController> _logger;
+
+        public HookController(ILogger<HookController> logger)
+        {
+            this._logger = logger;
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public async Task<ActionResult<HookResponse>> NewPR()
+        {
+            return new HookResponse();
+        }
+    }
+}
