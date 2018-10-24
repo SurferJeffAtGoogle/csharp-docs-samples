9046719b92c469556cd30ccbfff61974bc04d914
Jeffrey Rennie <rennie@google.com>
2018-10-24 10:19:40 -0700

    Simple logging project.
    Change-Id: Idb48ccbbba5a6fa8c696830c0e0f867bcd2ee18b

---
+++ logging/api/AspNetCore/Startup.cs
@@ -0,0 +1,33 @@
+﻿using Google.Cloud.Diagnostics.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace WebApp
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env,
+            ILoggerFactory loggerFactory)
+        {
+            // Log to Google.
+            loggerFactory.AddGoogle("YOUR-PROJECT-ID");
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
