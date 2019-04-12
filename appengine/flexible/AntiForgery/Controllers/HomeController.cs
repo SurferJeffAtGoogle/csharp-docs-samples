f2345377c885dc6d76a274a6c6c1ef36a979505c
Jeffrey Rennie <rennie@google.com>
2019-04-12 15:03:53 -0700

    A pure antiforgrery sample.
    Change-Id: I021a7085e3c9b4cf002d4a85cc240fba8df99533

---
+++ appengine/flexible/AntiForgery/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AntiForgery.Models;
+using Microsoft.Extensions.Options;
+
+namespace AntiForgery.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Index(HomeIndexViewModel model)
+        {
+            return View(model);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ appengine/flexible/AntiForgery/Models/HomeIndexViewModel.cs
@@ -0,0 +1,7 @@
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+public class HomeIndexViewModel 
+{
+    // The name of the person to be greeted.
+    public string Name { get; set; }
+}
