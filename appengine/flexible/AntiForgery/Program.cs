b6734afb2784b74beb90acd69cf7d5227085d2e6
Jeffrey Rennie <rennie@google.com>
2019-04-12 15:48:20 -0700

    Add diagnostics.
    Change-Id: I3515462af035bd76c8cca8b5ef01ff5d33e73e59

--- appengine/flexible/AntiForgery/Program.cs
+++ appengine/flexible/AntiForgery/Program.cs
@@ -7,6 +7,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace AntiForgery
@@ -20,6 +21,25 @@ namespace AntiForgery
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseDiagnostics()
                 .UseStartup<Startup>();
+    }
+
+    internal static class ProgramExtensions 
+    {
+        internal static IWebHostBuilder UseDiagnostics(
+            this IWebHostBuilder builder)
+        {
+            string projectId = Environment
+                .GetEnvironmentVariable("GOOGLE_CLOUD_PROJECT");
+            if (!string.IsNullOrWhiteSpace(projectId)) {
+                string service = Environment
+                    .GetEnvironmentVariable("GAE_SERVICE") ?? "unknown";
+                string version = Environment
+                    .GetEnvironmentVariable("GAE_VERSION") ?? "unknown";
+                builder.UseGoogleDiagnostics(projectId, service, version);
+            }            
+            return builder;
+        }
     }
 }
